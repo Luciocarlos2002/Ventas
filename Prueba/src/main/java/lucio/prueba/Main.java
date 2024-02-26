050da819d90738d83f7b5a26f4f3149359fbe17a
Luciocarlos2002 <lucio.huaman@vallegrande.edu.pe>
2024-02-25 20:10:57 -0500

    Agregando constante

--- Prueba/src/main/java/lucio/prueba/Main.java
+++ Prueba/src/main/java/lucio/prueba/Main.java
@@ -1,17 +1,20 @@
 package lucio.prueba;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static lucio.prueba.constantes.Constantes.CINCO;
+import static lucio.prueba.constantes.Constantes.UNO;
+
+import lucio.prueba.constantes.Constantes;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.print(Constantes.MENSAJE);
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = UNO; i <= CINCO; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println(Constantes.MENSAJE_2 + i);
         }
     }
 }
---
+++ Prueba/src/main/java/lucio/prueba/constantes/Constantes.java
@@ -0,0 +1,8 @@
+package lucio.prueba.constantes;
+
+public class Constantes {
+    public static final String MENSAJE = "Hello and welcome!";
+    public static final String MENSAJE_2 = "i = ";
+    public static final Integer UNO = 1;
+    public static final Integer CINCO = 5;
+}
